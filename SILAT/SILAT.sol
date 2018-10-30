bab19ef967a51c41f57cdcf2d7e21c7d764c8afb
Kendall Kant <kent2696@hotmail.com>
2018-10-30 05:28:53 -0500

    type: refactor, style, docs - variables names and mapping

--- SILAT/SILAT.sol
+++ SILAT/SILAT.sol
@@ -1,61 +1,63 @@
 pragma solidity 0.4.25;
 
-//Sistema de Licitaciones Abiertas y Transparentes v0.1.0
-
-//ADVERTENCIA/WARNING este archivo es una interpretacion muy basica de lo minimo que debe tener el programa
-//tiene combinado nombres en castellano y en ingles porque yolo, quedan advertidos
-
 contract silat
 {
     //Estructura que almacena la informacion de cada bidder
     struct Bidder
     {
         uint256 id_bidder;
-        string bidder_name;
-        address direccion_bidder;
+        string name;
+        address bidder_address;
     }
 
-    //Estructura que almacena la informacion de la licitacion actual
-    struct Licitacion
+    //Estructura que almacena la informacion de la Bid actual
+    struct Bid
     {
-        uint256 id_licitacion;
-        uint256 presupuesto;
-        bool registro_terminado;
+        uint256 id_bid;
+        uint256 budget;
+        bool registration_finished;
 
-        uint256 bidder_ganador;
-        Bidder[] bidders;
-        uint256 bidderCount;
+        uint256 id_winner;
+        mapping(uint256 => Bidder) bidders;
+        uint256 bidders_count;
     }
 
-    uint256 public licitacion_count;//Variable que nos almacena la cantidad de licitaciones que se han agregado al smart contract hasta ahora
+    uint256 public bid_count;//Variable que nos almacena la cantidad de bids que se han agregado al smart contract hasta ahora
 
-    constructor() public
+    constructor() public //metodo constructor
     {
-        licitacion_count = 0;
+        bid_count = 0;
     }
     
-    mapping(uint256 => Licitacion) public licitaciones; //Declaramos el arreglo de licitaciones
+    mapping(uint256 => Bid) public bids; //Declaramos el arreglo de bids
     
-    function addLicitacion(uint256 id_licitacion, uint256 presupuesto) public //Funcion publica que agrega una nueva licitacion, no retorna nada
+    function addBid(uint256 id_bid, uint256 budget) public //Funcion publica que agrega una nueva Bid
     {
-        licitaciones[id_licitacion].id_licitacion = id_licitacion;
-        licitaciones[id_licitacion].presupuesto = presupuesto;
-        licitaciones[id_licitacion].registro_terminado = false;
-        //licitaciones[id_licitacion].bidder_ganador = 0; //solidity inicializa todo en 0 por defecto asi que nose si ponerle esto, pero sin esto funciona 
-        //licitaciones[id_licitacion].bidders = 0
+        bids[id_bid].id_bid = id_bid;
+        bids[id_bid].budget = budget;
+        bids[id_bid].registration_finished = false;
+        bids[id_bid].id_winner = 0;
         
-        licitacion_count++;
+        bid_count++;
     }
-    //De aqui para arriba todo ha sido probado en REMIX y corre relativamente bien
-    //De aqui para abajo nada ha sido probado, cuidado
 
-    //Funcion que le agrega un bidder a una licitacion en especifico
-    function addBidder(uint256 id_licitacion, uint256 id_bidder, string bidder_name) public
+    //Funcion que le agrega un bidder a una bid en especifico
+    function addBidder(uint256 id_bid, uint256 id_bidder, string name) public
     {
-        licitaciones[id_licitacion].bidders[id_bidder].id_bidder = id_bidder;
-        licitaciones[id_licitacion].bidders[id_bidder].bidder_name = bidder_name;
-        licitaciones[id_licitacion].bidders[id_bidder].direccion_bidder = msg.sender;
+        bids[id_bid].bidders[id_bidder].id_bidder = id_bidder;
+        bids[id_bid].bidders[id_bidder].name = name;
+        bids[id_bid].bidders[id_bidder].bidder_address = msg.sender;
 
-        licitaciones[id_licitacion].bidderCount++;
+        bids[id_bid].bidders_count++;
     }
+
+    /*TODO
+    agregar funcion que cambie el estado del registro de la licitacion a terminado
+    validacion de que si ya se termino el periodo de registro no puedan agregarse mas bidders
+    validar que no se puedan agregar bidders si no existe ninguna bid actualmente
+    agregar variable para identificar que la licitacion esta en curso
+    validar que el numero de id no sea 0, y que sea unico, al agregar bids
+    agregar variables para detalles de la licitacion
+    */
+    
 }
