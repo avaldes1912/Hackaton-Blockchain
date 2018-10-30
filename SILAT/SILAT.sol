55b0d784c2f36706cbca37ee1ca04296b1e5f6e1
Kendall Kant <kent2696@hotmail.com>
2018-10-30 14:10:46 -0500

    type: docs - cleared TODO's that were done

--- SILAT/SILAT.sol
+++ SILAT/SILAT.sol
@@ -1,27 +1,6 @@
 pragma solidity 0.4.25;
 
     /*TODO
-    (DONE) agregar variables para detalles de la licitacion
-    (DONE) agregar variable para identificar que la licitacion esta en curso
-    (DONE) agregar funcion que cambie el estado del registro de la licitacion a terminado
-    (DONE) agregar funcion que cambie el estado del curso de la licitacion a "ya no esta en curso"
-    (DONE) validacion de que si ya se termino el periodo de registro no puedan agregarse mas bidders
-    (DONE) validacion de que si ya la licitacion no esta ongoing (en curso) no puedan agregarse mas bidders
-    (DONE) validar que no se puedan solicitar terminar el periodo de registro ni el periodo de en curso si el numero de licitaciones es de 0
-    (DONE) endOngoingPeriod func return succes and error messages
-    (DONE) endRegistrationPeriod func return success and error messages
-    (DONE) addBidder func return success and error messages
-    (DONE) validar que no se puedan agregar bidders si no existe ninguna bid actualmente
-    (DONE) cambiar las validaciones de los if por los requires correspondientes
-    (DONE) agregar variables de estado a la estructura de las licitaciones
-    (DONE) validar que solo el que tenga la direccion de admin pueda agregar licitaciones
-    (DONE) diferenciar variables de parametros
-    (DONE) agregar variable de numero de partida, a la estructura bid
-    (DONE) agregar variables de puntuacion a los bidders
-    (DONE) asignar id automaticamente con contadores
-    (DONE) funcion para calificar a cada competidor (bidder)
-    (DONE) funcion para calcular el competidor (bidder) ganador
-
     reemplazar los requires repetidos por una sola funcion modifier
     agregar estructuras de fecha para controlar el periodo de registro e implementarlo con la estructura bid
     validar que el numero de id no sea 0, y que sea unico, al agregar bids
